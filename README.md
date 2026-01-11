9b3674c3606133b08eb12de37f5c62469b8eda6d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2026-01-11 13:25:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# object_box_poc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/object_box_poc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.object_box_poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
